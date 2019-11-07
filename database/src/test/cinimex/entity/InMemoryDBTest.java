90f066e1693f5a3bc94c9f68fcecf5ec03d9ea8c
potapovnr <potapovnr@gmail.com>
2019-11-07 13:03:11 +0300

    Работающая настройка тестов на junit5

--- database/src/test/cinimex/entity/InMemoryDBTest.java
+++ database/src/test/cinimex/entity/InMemoryDBTest.java
@@ -2,26 +2,20 @@ package cinimex.entity;
 
 import cinimex.JPArepository.BalanceRepository;
 import cinimex.conf.H2JpaConfig;
-import cinimex.entity.BalanceEntity;
-import lombok.experimental.ExtensionMethod;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import javax.annotation.Resource;
 import javax.transaction.Transactional;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(
         classes = { H2JpaConfig.class },
         loader = AnnotationConfigContextLoader.class)
-@Transactional
 public class InMemoryDBTest {
 
     @Resource
@@ -29,6 +23,7 @@ public class InMemoryDBTest {
 
     @Test
     public void givenStudent_whenSave_thenGetOk() {
+        System.out.println("lol blyat");
         BalanceEntity savedEntity = balanceRepository.save(new BalanceEntity());
     }
 }
